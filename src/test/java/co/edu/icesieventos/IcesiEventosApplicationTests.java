cc637bd4fc2841571b7d477253256c5ea8dab586
SanjoDB <1006110438@u.icesi.edu.co>
2024-05-16 22:30:57 -0500

    Back end Structure

---
+++ src/test/java/co/edu/icesieventos/IcesiEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.icesieventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcesiEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
